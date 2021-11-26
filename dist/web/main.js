a27cb9943e0d02fccd9829af5a3453b61c1b543d
danitheturtle <danitheturtle@protonmail.com>
2021-11-25 20:14:18 -0500

    Fixed dist not being in gitignore

--- dist/web/main.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkweb=self.webpackChunkweb||[]).push([[179],{970:(e,s,n)=>{n(378);var t=n(542),r=n(246);const c=()=>(0,r.jsx)("div",{children:"Testing! Is this thing on?"});t.render((0,r.jsx)(c,{}),document.getElementById("sneks"))}},e=>{e.O(0,[81],(()=>{return s=970,e(e.s=s);var s}));e.O()}]);
-//# sourceMappingURL=main.js.map
